30843d799935356de37d20a5b1fa1e77d8e78455
Arsh23 <arsh.bbps@gmail.com>
2016-01-11 01:24:34 +0530

    second draft

--- js/heading.js
+++ js/heading.js
@@ -64,6 +64,9 @@ function return_arr(str)
 		{
 			tempstr = str.substring(0,i) + random_string(len-i-1);
 			array.push(tempstr);
+			tempstr = str.substring(0,i) + random_string(len-i-1);
+			array.push(tempstr);
+
 		}		
 	}
 	
@@ -74,58 +77,102 @@ function return_arr(str)
 
 	return array;
 }
+var timer = 20;
+var delay = 200;
+var ids = [0,0,0,0,0];
+var array = [[],[],[],[],[]];
+var counter = [0,0,0,0,0];
 
-var array1 = [];
-var counter1 = 0;
-var str1 = '';
-var id1 = 0;
-
-var array2 = [];
-var counter2 = 0;
-var str2 = '';
-var id2 = 0;
 
 function animate_heading_1(elem)
 {
 	var $elem = $(elem);
-	if(counter1 == array1.length - 1)
+	if(counter[0] == array[0].length - 1)
 	{
-		clearInterval(id1);
+		clearInterval(ids[0]);
 	}
-	$elem.html(array1[counter1])
-	counter1 += 1
+	$elem.html(array[0][counter[0]])
+	counter[0] += 1
 }
 
 function animate_heading_2(elem)
 {
 	var $elem = $(elem);
-	if(counter2 == array2.length - 1)
+	if(counter[1] == array[1].length - 1)
 	{
-		clearInterval(id2);
+		clearInterval(ids[1]);
 	}
-	$elem.html(array2[counter2])
-	counter2 += 1
+	$elem.html(array[1][counter[1]])
+	counter[1] += 1
+}
+function animate_heading_3(elem)
+{
+	var $elem = $(elem);
+	if(counter[2] == array[2].length - 1)
+	{
+		clearInterval(ids[2]);
+	}
+	$elem.html(array[2][counter[2]])
+	counter[2] += 1
+}
+function animate_heading_4(elem)
+{
+	var $elem = $(elem);
+	if(counter[3] == array[3].length - 1)
+	{
+		clearInterval(ids[3]);
+	}
+	$elem.html(array[3][counter[3]])
+	counter[3] += 1
+}
+function animate_heading_5(elem)
+{
+	var $elem = $(elem);
+	if(counter[4] == array[4].length - 1)
+	{
+		clearInterval(ids[4]);
+	}
+	$elem.html(array[4][counter[4]])
+	counter[4] += 1
 }
 
 function main()
 {
-	if(isScrolledIntoView('.sub-heading-1') == true && id1==0)
+	if(isScrolledIntoView('.sub-heading-1') == true && ids[0]==0)
 	{
-		counter1 = 0
-		str1 = ''
-		array1 = return_arr('REGISTRATION')
-		id1 = setInterval( function() { animate_heading_1('.sub-heading-1'); }, 20);
+		counter[0] = 0
+		array[0] = return_arr('SCHEDULE')
+		setTimeout(function(){ids[0] = setInterval( function() { animate_heading_1('.sub-heading-1'); }, timer);},delay);
 		
 	}
-	if(isScrolledIntoView('.sub-heading-2') == true && id2==0)
+	if(isScrolledIntoView('.sub-heading-2') == true && ids[1]==0)
 	{
-		counter2 = 0
-		str2 = ''
-		array2 = return_arr('FAQ')
-		id2 = setInterval( function() { animate_heading_2('.sub-heading-2'); }, 20);
+		counter[1] = 0
+		array[1] = return_arr('FAQ')
+		setTimeout(function(){ids[1] = setInterval( function() { animate_heading_2('.sub-heading-2'); }, timer);},delay);
 		
 	}
-	
+	if(isScrolledIntoView('.sub-heading-3') == true && ids[2]==0)
+	{
+		counter[2] = 0
+		array[2] = return_arr('ABOUT')
+		setTimeout(function(){ids[2] = setInterval( function() { animate_heading_3('.sub-heading-3'); }, timer);},delay);
+		
+	}
+	if(isScrolledIntoView('.sub-heading-4') == true && ids[3]==0)
+	{
+		counter[3] = 0
+		array[3] = return_arr('SPONSORS')
+		setTimeout(function(){ids[3] = setInterval( function() { animate_heading_4('.sub-heading-4'); }, timer);},delay);
+		
+	}
+	if(isScrolledIntoView('.sub-heading-5') == true && ids[4]==0)
+	{
+		counter[4] = 0
+		array[4] = return_arr('CONACT')
+		setTimeout(function(){ids[4] = setInterval( function() { animate_heading_5('.sub-heading-5'); }, timer);},delay);
+		
+	}
 }
 
 
